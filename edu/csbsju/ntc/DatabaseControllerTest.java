e3dbefd5abed1a185cb4efeff8d012011a7d5604
JHEIDELBE001 <jheidelbe001@vplincsci015.ad.csbsju.edu>
2018-04-08 15:23:39 -0500

    Initial Tests commit

---
+++ edu/csbsju/ntc/DatabaseControllerTest.java
@@ -0,0 +1,19 @@
+package edu.csbsju.ntc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DatabaseControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
